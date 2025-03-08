fd368c801492950dbb33344022bdb2058cf2c6bb
boby255 <das19sujata@gmail.com>
2025-03-08 08:39:56 +0530

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -9,7 +9,7 @@ function UserDetails() {
   useEffect(() => {
     async function fetchUser() {
       const response = await fetch(
-        https://jsonplaceholder.typicode.com/users/${id}
+        `https://jsonplaceholder.typicode.com/users/${id}`
       );
       const data = await response.json();
       setUser(data);
